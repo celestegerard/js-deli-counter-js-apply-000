dd3f88e9559231a3f9acd9e38fc455fd55decf66
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-30 20:18:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function takeANumber(katzDeliLine) {
     return `Welcome, ${count}. You are number ${katzDeliLine.length} in line.`;
   }
 }
+
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "There is nobody waiting to be served!";
