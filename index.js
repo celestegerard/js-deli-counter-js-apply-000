4c445d344618adf7b025ed59561c64958d34afb4
Celeste Gerard <celeste.gerard@gmail.com>
2018-01-30 20:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var katzDeliLine = [];
 
-function takeANumber(katzDeliLine, name) {
-katzDeliLine.push(name);
-return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
-}
+
+  
 
 function nowServing (katzDeliLine) {
   if (katzDeliLine.length === 0) {
@@ -14,19 +12,17 @@ function nowServing (katzDeliLine) {
   }
 }
 
-var counter = [];
 
 function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
+    var counter = [];
     for (var i = 0; i < katzDeliLine.length; i++) {
       counter.push(" "+[i+1]+". "+ katzDeliLine[i]);
     }
      return "The line is currently:"+counter;
   }
 }
-
-
 
 
